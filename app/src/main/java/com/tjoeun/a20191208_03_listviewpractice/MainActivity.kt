176eb9573e48de9791a65b7d5030095709dace73
미현 김 <rlaalgus132@naver.com>
2019-12-08 14:59:35 +0900

    액티비티에서 어댑터 / 리스트뷰 연결

--- app/src/main/java/com/tjoeun/a20191208_03_listviewpractice/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191208_03_listviewpractice/MainActivity.kt
@@ -3,11 +3,14 @@ package com.tjoeun.a20191208_03_listviewpractice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.tjoeun.a20191208_03_listviewpractice.adapters.UserAdapter
 import com.tjoeun.a20191208_03_listviewpractice.datas.User
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val studentList = ArrayList<User>()
+    var userAdapter:UserAdapter?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +19,8 @@ class MainActivity : AppCompatActivity() {
 //        학생 목록 추가
         addStudents()
 
+        userAdapter = UserAdapter(this,R.layout.user_list_item,studentList)
+        userListView.adapter = userAdapter
     }
 
     fun addStudents(){
