7ef2fe87ebfff03b08013d56b4205b927d6b42b5
미현 김 <rlaalgus132@naver.com>
2019-12-08 14:44:44 +0900

    사용자 목록 배열에 추가 - 별개 함수로 따로 빼냄

--- app/src/main/java/com/tjoeun/a20191208_03_listviewpractice/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191208_03_listviewpractice/MainActivity.kt
@@ -7,21 +7,30 @@ import com.tjoeun.a20191208_03_listviewpractice.datas.User
 
 class MainActivity : AppCompatActivity() {
 
+    val studentList = ArrayList<User>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tempUser = User("이름","서울시",false)
-        Log.d("사용자주소",tempUser.address)
-
-        val tempUser2 = User()
-        Log.d("사용자 주소", tempUser2.address)
-
-
-        val tempUser3 = User("아이유")
-        Log.d("거주지",tempUser3.address)
+//        학생 목록 추가
+        addStudents()
 
     }
 
+    fun addStudents(){
+
+        studentList.add(User("계석준", "서울시 도봉구", false))
+        studentList.add(User("김미현", "경기도 군포시", true))
+        studentList.add(User("김영호", "서울시 은평구", false))
+        studentList.add(User("노혜진", "서울시 강동구", true))
+        studentList.add(User("류찬울", "서울시 도봉구", false))
+        studentList.add(User("양성심", "서울시 관악구", true))
+        studentList.add(User("이규현", "서울시 도봉구", false))
+        studentList.add(User("이수정", "경기도 고양시", true))
+        studentList.add(User("차순혁", "서울시 구로구", false))
+        studentList.add(User("황현하", "경기도 성남시", true))
+        studentList.add(User("조경진", "서울시 은평구", false))
+    }
 
 }
