faef1780b3e04f6b01052d014998c72343311e22
미현 김 <rlaalgus132@naver.com>
2019-12-08 15:08:42 +0900

    각 줄마다 올바른 데이터 뿌려주기

--- app/src/main/java/com/tjoeun/a20191208_03_listviewpractice/adapters/UserAdapter.kt
+++ app/src/main/java/com/tjoeun/a20191208_03_listviewpractice/adapters/UserAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import com.tjoeun.a20191208_03_listviewpractice.R
 import com.tjoeun.a20191208_03_listviewpractice.datas.User
 
@@ -22,7 +23,25 @@ class UserAdapter (context: Context, resid:Int, list:ArrayList<User>) : ArrayAda
         val row = tempRow!!
         
 //        상황에 맞는 데이터를 user_list_item에 뿌려주기
-        
+
+//        리스트에서 각 줄에 맞는 근거 데이터 뽑아오기
+//        position 변수가 몇번째 줄을 출력하는지 알려주는 역할.
+        val data = mList.get(position)
+
+//        각 줄에서 데이터를 뿌려줄 뷰를 찾아다 연결.
+
+        val userNameTxt=row.findViewById<TextView>(R.id.userNameTxt)
+        val userAddressTxt = row.findViewById<TextView>(R.id.userAddressTxt)
+        val genderTxt = row.findViewById<TextView>(R.id.genderTxt)
+
+        userNameTxt.text = data.name
+        userAddressTxt.text = "(${data.address})"
+        if(data.isWomen){
+            genderTxt.text ="여성"
+        }else{
+            genderTxt.text="남성"
+        }
+
         return row
     }
     
