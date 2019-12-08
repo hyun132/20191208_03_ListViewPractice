d97087065a1065b7b37461c105466d5ac321099d
미현 김 <rlaalgus132@naver.com>
2019-12-08 14:03:23 +0900

    보조생성자 추가

--- app/src/main/java/com/tjoeun/a20191208_03_listviewpractice/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191208_03_listviewpractice/MainActivity.kt
@@ -13,5 +13,8 @@ class MainActivity : AppCompatActivity() {
 
         val tempUser = User("이름","서울시",false)
         Log.d("사용자주소",tempUser.address)
+
+        val tempUser2 = User()
+        Log.d("사용자 주소", tempUser2.address)
     }
 }
--- app/src/main/java/com/tjoeun/a20191208_03_listviewpractice/datas/User.kt
+++ app/src/main/java/com/tjoeun/a20191208_03_listviewpractice/datas/User.kt
@@ -4,4 +4,7 @@ class User(inputName:String, inputAddress:String, isWomenOk:Boolean){
     var name=inputName
     var address=inputAddress
     var isWomen=isWomenOk
+
+    constructor() : this("미정","거주지 불명", false)
+
 }
